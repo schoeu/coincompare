6ade4ef3be2d67ef972cd01b79e42afa8e3f4a41
schoeu89 <schoeu89@gmail.com>
2018-05-12 20:02:26 +0800

    finish usercoin api.

--- actions/usercoin.go
+++ actions/usercoin.go
@@ -9,7 +9,7 @@ import (
 )
 
 type userCoin struct {
-	Count string   `json:"count"`
+	Count int      `json:"count"`
 	Win   []string `json:"win"`
 	Lose  []string `json:"lose"`
 	Rate  string   `json:"rate"`
@@ -17,9 +17,9 @@ type userCoin struct {
 
 // 获取流量信息
 func GetUserCoinInfo(c *gin.Context, _ *sql.DB, compareDB *sql.DB) {
-	var pid, symbol string
-	var ci coinsInfo
-	var ciCtt []coinsInfo
+	var name string
+	var state int
+	var uc userCoin
 
 	code := c.PostForm("code")
 	iv := c.PostForm("iv")
@@ -29,16 +29,21 @@ func GetUserCoinInfo(c *gin.Context, _ *sql.DB, compareDB *sql.DB) {
 
 		rows, err := compareDB.Query("SELECT coin_name, state FROM bt_coincom.bt_coininfo where uid = ?", userRawInfo.UnionId)
 		utils.ErrHandle(err)
-
+		count := 0
 		for rows.Next() {
-			err := rows.Scan(&pid, &symbol)
+			err := rows.Scan(&name, &state)
 			utils.ErrHandle(err)
 
-			ci.Pid = pid
-			ci.Name = symbol
-
-			ciCtt = append(ciCtt, ci)
+			if state == 0 {
+				uc.Lose = append(uc.Lose, name)
+			} else {
+				uc.Win = append(uc.Win, name)
+			}
+			count++
 		}
+		uc.Count = count
+		uc.Rate = fmt.Sprintf("%.1f", float64(len(uc.Win))/float64(count)*100)
+
 		err = rows.Err()
 		utils.ErrHandle(err)
 		defer rows.Close()
@@ -47,6 +52,6 @@ func GetUserCoinInfo(c *gin.Context, _ *sql.DB, compareDB *sql.DB) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": 0,
 		"msg":    "ok",
-		"data":   ciCtt,
+		"data":   uc,
 	})
 }
